97acd90423388830e195be9ae84d61560da4bfb1
Alex <alex781014@gmail.com>
2022-07-25 17:16:17 +0800

    save Canvas to localStroage

--- src/Customized_product/Customized_product.js
+++ src/Customized_product/Customized_product.js
@@ -2,9 +2,9 @@ import Foods from './Components/Foods'
 import Canvas from './Components/Canvas'
 import SelectedFood from './Components/SelectedFood'
 import './Customized_product.css'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import foodList from './data/foodList.json'
-import axios from 'axios'
+// import axios from 'axios'
 //初始化狀態用的函式
 // const initState = (productArray) => {
 //   const state = []
@@ -25,19 +25,19 @@ function Customized_product() {
   const [dataFromFoodArea, setDataFromFoodArea] = useState([])
   const [foodCount, setFoodCount] = useState(1)
   const [cart, setCart] = useState([])
-  const [userData, setUserData] = useState([])
+  // const [userData, setUserData] = useState([]) //撈資料庫
 
-  const getUserData = async () => {
-    const response = await axios.get(
-      'http://localhost:3600/customized_lunch/api'
-    )
-    console.log(response.data)
+  // const getUserData = async () => {
+  //   const response = await axios.get(
+  //     'http://localhost:3600/customized_lunch/api'
+  //   )
+  //   console.log(response.data)
 
-    setUserData(response.data)
-  }
+  //   setUserData(response.data)
+  // }
 
   useEffect(() => {
-    getUserData()
+    // getUserData()
   }, [])
 
   const calcTotalPrice = () => {
