051a8ff904940101dcf1a909641e65ee7cb5daf0
Alex <alex781014@gmail.com>
2022-07-26 23:24:06 +0800

    changname

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Customized_product from './Customized_product/Customized_product'
+import CustomizedLunch from './Customized_lunch/CustomizedLunch'
 // import ThxTeacher from "./Customized_product/Components/ThxTeacher"
 // import Demo from './Customized_product/Components/Demo'
 function App() {
-  return <Customized_product />
+  return <CustomizedLunch />
   // return <Demo />
 }
 
